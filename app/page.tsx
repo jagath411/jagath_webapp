50d5d450756ab6809a8f46be60b2b252713e4f93
Jagath K R <108512623+jagath411@users.noreply.github.com>
2024-08-15 18:50:04 +0530

    Spotlight Start

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { Hero } from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main
+      className="relative bg-black flex justify-center items-center flex-col 
+      overflow-hidden mx-auto sm:px-10 px-5"
+    >
       <div>
-        <h1>Hello Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
