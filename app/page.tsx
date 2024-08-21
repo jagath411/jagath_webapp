4ddc63e943faeefb76dc483245c8aedc4189d080
Jagath K R <108512623+jagath411@users.noreply.github.com>
2024-08-21 21:55:13 +0530

    Grid Section Strat

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,7 +11,11 @@ export default function Home() {
       overflow-hidden mx-auto sm:px-10 px-5"
     >
       <div>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
