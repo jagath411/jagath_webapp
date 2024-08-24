07f666feb8962d46a51c54507d9003a299a247eb
Jagath K R <108512623+jagath411@users.noreply.github.com>
2024-08-24 21:52:37 +0530

    Row Span Fix in the bentogrid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BentoGrid } from "./ui/BentoGrid";
+import { BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
